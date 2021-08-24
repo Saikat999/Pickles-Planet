6272d7f53ec03216948f8570ff2b0632d9d7e1ed
Saikat Hasan <saikathasan005@gmail.com>
2021-08-25 00:38:06 +0600

    Slider image uploaded & added Footer

--- resources/views/pickle-home.blade.php
+++ resources/views/pickle-home.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('../css/home.css')}}">
 
     <title>Pickles Planet</title>
 </head>
@@ -13,45 +14,82 @@
     
      <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-info">
-            <a class="navbar-brand text-white" href="#">Pickles Planet</a>
+            <a class="navbar-brand text-white font-weight-bold" href="#">Pickles Planet</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto text-white">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About us</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Pickles
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Sweet Pickles</a>
-                    <a class="dropdown-item" href="#">Sour Pickles</a>
-                    <a class="dropdown-item" href="#">Mixed Pickles</a>
-                    
-                </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About us</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Pickles
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Sweet Pickles</a>
+                        <a class="dropdown-item" href="#">Sour Pickles</a>
+                        <a class="dropdown-item" href="#">Mixed Pickles</a>
+                        
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
                 
                 </ul>
-               <button type="button" class="btn btn-warning mx-2">Login</button>
-               <button type="button" class="btn btn-warning">Register</button>
+
+
+               <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
+               </form>
             </div>
         </nav>
 
-        <div class="pickle-body">
-            <h1 class="text-center mt-5">Pickles Planet</h1>
-            <p class="text-center font-italic">Hunger the best PICKLE</p>
+      <div class="slider-image">
+          <div class="container">
+              <div class="row">
+                
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                            <img class="d-block w-100" src="{{asset('../images/pickle-bg4.png')}}" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="{{asset('../images/bg-2.png')}}" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="{{asset('../images/bg-3.png')}}" alt="Third slide">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+              
+              </div>
+          </div>
+      </div>
+
+        <div class="pickle-items">
+           <h3 class="text-center text-white" style="padding-top: 15px;">All Pickle Items</h3>
         </div>
+        <div class="footer">
+             <p class="text-center text-white" style="padding-top: 8px">Copy @ 2021. All rights reserved</p>
+        </div>
+        
          
      </div>
-
-
-
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
